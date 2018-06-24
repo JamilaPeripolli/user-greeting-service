181f39cc68e5143452b3d94db9ae2fb7c8f8821b
Jam <milaperipolli@gmail.com>
2018-06-24 19:33:28 -0300

    Updating server.port

--- src/main/java/com/jps/example/usergreetingservice/config/RibbonConfig.java
+++ src/main/java/com/jps/example/usergreetingservice/config/RibbonConfig.java
@@ -4,7 +4,9 @@ import com.netflix.client.config.IClientConfig;
 import com.netflix.loadbalancer.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class RibbonConfig {
 
     @Autowired
