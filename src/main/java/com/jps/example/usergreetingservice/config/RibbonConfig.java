ac50c28e129dcd02738a564dcc99e1f98f47fd07
Jam <milaperipolli@gmail.com>
2018-06-24 19:39:59 -0300

    Enabling integration with Eureka

--- src/main/java/com/jps/example/usergreetingservice/config/RibbonConfig.java
+++ src/main/java/com/jps/example/usergreetingservice/config/RibbonConfig.java
@@ -4,19 +4,12 @@ import com.netflix.client.config.IClientConfig;
 import com.netflix.loadbalancer.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class RibbonConfig {
 
     @Autowired
     IClientConfig ribbonClientConfig;
 
-    @Bean
-    public IPing ribbonPing(IClientConfig config) {
-        return new NoOpPing();
-    }
-
-    @Bean
-    public IRule ribbonRule(IClientConfig config) {
-        return new RoundRobinRule();
-    }
 }
