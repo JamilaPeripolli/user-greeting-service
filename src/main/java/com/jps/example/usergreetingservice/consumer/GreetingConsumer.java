cedf7a9e6481a478e0e89a9bcfbfccab5be167f0
JamilaPeripolli <32621357+JamilaPeripolli@users.noreply.github.com>
2018-07-01 23:34:21 -0300

    Adding Circuit Breaker pattern (#3)

--- src/main/java/com/jps/example/usergreetingservice/consumer/GreetingConsumer.java
+++ src/main/java/com/jps/example/usergreetingservice/consumer/GreetingConsumer.java
@@ -1,5 +1,6 @@
 package com.jps.example.usergreetingservice.consumer;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -21,8 +22,13 @@ public class GreetingConsumer {
         this.restTemplate = restTemplate;
     }
 
+    @HystrixCommand(fallbackMethod = "getDefaultGreeting")
     public String getRandomGreeting() {
         String greeting = restTemplate.getForObject("http://greeting-service" + greetingEndpointUri, String.class);
         return greeting;
+    }
+
+    public String getDefaultGreeting() {
+        return "Good bye";
     }
 }
